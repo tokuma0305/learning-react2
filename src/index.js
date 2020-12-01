900c6daf0ab0f30318a09034ae7609c3516262d8
“tokuma0305” <yoshinari.tokuma.infratop@gmail.com>
2020-12-01 23:22:25 +0900

    components

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <App title="App" message="This is App Component!" />,
+  <App />,
   document.getElementById('root')
 );
 
